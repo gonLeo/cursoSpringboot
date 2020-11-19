08caeda922a19551487d9366164b5fb9b9c7ec58
Leonardo Gonçalves de Oliveira <leonardo.g.o.lrv@gmail.com>
2020-11-19 11:01:38 -0400

    Ajustando pastas

--- mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -3,6 +3,7 @@ package com.appsdeveloperblog.app.ws.ui.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -48,8 +49,11 @@ public class UserController {
 	public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) throws Exception {
 		UserRest returnValue = new UserRest();
 
-		UserDto userDto = new UserDto();
-		BeanUtils.copyProperties(userDetails, userDto);
+//		UserDto userDto = new UserDto();
+//		BeanUtils.copyProperties(userDetails, userDto);
+		ModelMapper modelMapper = new ModelMapper();
+		UserDto userDto = modelMapper.map(userDetails, UserDto.class);
+
 
 		UserDto createdUser = userService.createUser(userDto);
 		BeanUtils.copyProperties(createdUser, returnValue);
@@ -90,7 +94,7 @@ public class UserController {
 		List<UserRest> returnValue = new ArrayList<>();
 
 		List<UserDto> users = userService.getUsers(page, limit);
-		
+
 		for (UserDto userDto : users) {
 			UserRest userModel = new UserRest();
 			BeanUtils.copyProperties(userDto, userModel);
